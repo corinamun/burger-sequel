9dcae9332e552594c323f2bf6efe9fd13d525f3b
Corina Mun <corinamun@gmail.com>
2017-10-05 00:04:58 -0700

    final commit

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -19,23 +19,35 @@ router.get("/burgers", function(req, res) {
 
 // post route -> back to index
 router.post("/burgers/create", function(req, res) {
+  db.Burger.create({
+    burger_name: req.body.burger_name
+  }).then(function(){
+    res.redirect("/");
+  })
   // takes the request object using it as input for buger.addBurger
-  burger.create(req.body.burger_name, function(result) {
+    // burger.create(req.body.burger_name, function(result) {
     // wrapper for orm.js that using MySQL insert callback will return a log to console,
     // render back to index with handle
-    console.log(result);
-    res.redirect("/");
-  });
+  //   console.log(result);
+  //   res.redirect("/");
+  // });
+
 });
 
 // put route -> back to index
 router.put("/burgers/update", function(req, res) {
-  burger.update(req.body.burger_id, function(result) {
+  db.Burger.update({
+    devoured: true
+  }, {where: { id: req.body.burger_id}
+}).then(function(){
+  res.redirect("/");
+})
     // wrapper for orm.js that using MySQL update callback will return a log to console,
     // render back to index with handle
-    console.log(result);
-    res.redirect("/");
-  });
+  //   console.log(result);
+  //   res.redirect("/");
+  // });
+
 });
 
 module.exports = router;
